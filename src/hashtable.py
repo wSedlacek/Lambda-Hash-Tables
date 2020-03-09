5f256b4c6e9559c7095fbecd7aafdf74e86c81fd
William Sedlacek <wsedlacekc@gmail.com>
2020-03-09 12:20:00 -0500

    Hash Table -- MVP
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/hashtable.py
+++ src/hashtable.py
@@ -1,3 +1,5 @@
+from typing import List
+
 # '''
 # Linked List hash table key/value pair
 # '''
@@ -12,12 +14,13 @@ class HashTable:
     A hash table that with `capacity` buckets
     that accepts string keys
     '''
-    def __init__(self, capacity):
+    def __init__(self, capacity: int):
+        self.items = 0
         self.capacity = capacity  # Number of buckets in the hash table
-        self.storage = [None] * capacity
+        self.storage: List[LinkedPair] = [None] * capacity
 
 
-    def _hash(self, key):
+    def _hash(self, key: str):
         '''
         Hash an arbitrary key and return an integer.
 
@@ -26,7 +29,7 @@ class HashTable:
         return hash(key)
 
 
-    def _hash_djb2(self, key):
+    def _hash_djb2(self, key: str):
         '''
         Hash an arbitrary key using DJB2 hash
 
@@ -35,7 +38,7 @@ class HashTable:
         pass
 
 
-    def _hash_mod(self, key):
+    def _hash_mod(self, key: str):
         '''
         Take an arbitrary key and return a valid integer index
         within the storage capacity of the hash table.
@@ -43,48 +46,98 @@ class HashTable:
         return self._hash(key) % self.capacity
 
 
-    def insert(self, key, value):
+    def insert(self, key: str, value: any):
         '''
         Store the value with the given key.
 
         Hash collisions should be handled with Linked List Chaining.
-
-        Fill this in.
         '''
-        pass
+        hashed_key = self._hash_mod(key)
+        linked_list = self.storage[hashed_key]
+
+        if not linked_list:
+            self.storage[hashed_key] = LinkedPair(key, value)
+            self.items += 1
+            # if self.items / self.capacity > 0.7:
+            #     self.resize()
+
+        else:
+            while linked_list.next and linked_list.key != key:
+                linked_list = linked_list.next
+
+            if linked_list.key == key:
+                linked_list.value = value
+            else:
+                linked_list.next = LinkedPair(key, value)
+
+                self.items += 1
+                # if self.items / self.capacity > 0.7:
+                #     self.resize()
 
 
-
-    def remove(self, key):
+    def remove(self, key: str):
         '''
         Remove the value stored with the given key.
 
         Print a warning if the key is not found.
-
-        Fill this in.
         '''
-        pass
+        hashed_key = self._hash_mod(key)
+        trailing_node = self.storage[hashed_key]
+        leading_node = trailing_node.next
+
+        if trailing_node and trailing_node.key == key:
+            self.storage[hashed_key] = trailing_node.next
+            self.items -= 1
+            # if self.items / self.capacity > 0.7:
+            #     self.resize(up=False)
+            return
+
+        while leading_node and leading_node.next and leading_node.key != key:
+            trailing_node = trailing_node.next
+            leading_node = leading_node.next
+
+        if leading_node and leading_node.key == key:
+            trailing_node.next = leading_node.next if leading_node else None
+            self.items -= 1
+            # if self.items / self.capacity > 0.7:
+            #     self.resize(up=False)
+            return
+
+        print("Value not found...")
 
 
-    def retrieve(self, key):
+
+    def retrieve(self, key: str):
         '''
         Retrieve the value stored with the given key.
 
         Returns None if the key is not found.
-
-        Fill this in.
         '''
-        pass
+        hashed_key = self._hash_mod(key)
+        linked_list = self.storage[hashed_key]
+
+        while linked_list and linked_list.next and linked_list.key != key:
+            linked_list = linked_list.next
+
+        return linked_list.value if  linked_list and linked_list.key == key else None
 
 
-    def resize(self):
+    def resize(self, up=True):
         '''
-        Doubles the capacity of the hash table and
+        Doubles or Halves the capacity of the hash table and
         rehash all key/value pairs.
-
-        Fill this in.
         '''
-        pass
+        items: List[LinkedPair] = [*self.storage]
+        self.capacity = int(self.capacity * 2 if up else self.capacity // 2)
+        self.storage = [None] * self.capacity
+
+        for item in items:
+            while item:
+                self.insert(item.key, item.value)
+                item = item.next
+
+
+
 
 
 
